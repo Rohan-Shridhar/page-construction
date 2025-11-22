7e53f9a949477a9832d503b48f970b4304ea4639
Rohan-Shridhar <rohansm668@gmail.com>
2025-11-23 00:58:22 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import DigitalClock from "./DigitalClock.jsx";
+import construction from "./assets/construct.gif";
+function App() {
+  return (
+    <>
+      <div className="Container">
+        <img src={construction} alt="Under Construction" className="construction-gif" />
+        <h1>Page Still Under Construction 🚧</h1>
+      </div>
+    </>
+  );
+}
+export default App;
